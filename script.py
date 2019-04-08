b8a6886ae3cf8cf392b3a6c5014f4f816dc20cb1
Nyctef <nyctef@nyctef.com>
2019-04-08 13:27:22 +0100

    starting to get to an end solution

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 import requests
 import xml.etree.ElementTree as ET
-import json
+from jsoncomment import JsonComment
 from datetime import datetime, timedelta
+from pprint import pprint
 
 
 class Bunch:
@@ -13,20 +14,51 @@ def get_lightboard_config():
     try:
         overall = Bunch(projects=set(), bts=set(), ignored_bts=set())
         with open('./BoardConfig.json') as config_file:
-            config_data = json.load(config_file)
+            # we use jsoncomment instead of stdlib json since the file contains trailing commas
+            config_data = JsonComment().load(config_file)
             for entry in config_data:
-                overall.projects.update(entry["Projects"])
-                overall.bts.update(entry["Buildtypes"])
-                overall.ignored_bts.update(entry["BuildtypesIgnore"])
+                overall.projects.update(entry.get("Projects", []))
+                overall.bts.update(entry.get("Buildtypes", []))
+                overall.ignored_bts.update(entry.get("BuildtypesIgnore", []))
         return Bunch(overall=overall)
-    except:
+    except Exception as e:
+        print(repr(e))
         print("Failed to load BoardConfig.json: skipping")
         return None
 
 
-def get_tc_builds(args):
+def get_tc_builds(username, password, projectId=None, buildTypeId=None):
+    baseurl = f"https://{args.username}:{args.password}@buildserver.red-gate.com/httpAuth/app/rest/"
+    buildsUrl = f"{baseurl}builds/"
+    maxResultCount = 4000
+
+    now = datetime.utcnow()
+    weekago = now - timedelta(days=7)
+    sinceDate = weekago.strftime("%Y%m%dT%H%M%S+0000")
+
+    locator = f"count:{maxResultCount},sinceDate:{sinceDate}"
+    if buildTypeId is not None:
+        locator += ",buildType:id:"+buildTypeId
+    elif projectId is not None:
+        locator += ",project:id:"+projectId
+
+    buildsResponse = requests.get(
+        buildsUrl, params={"locator": locator})
+    if not buildsResponse:
+        raise Exception("Error from teamcity: " + buildsResponse.text)
+
+    buildsResult = ET.fromstring(buildsResponse.text)
+    if int(buildsResult.attrib["count"]) == maxResultCount:
+        raise Exception("too many build results and paging isn't implemented")
+
+    return buildsResult.findall("build")
+
+
+def get_all_tc_builds(args):
 
     lightboard_config = get_lightboard_config()
+    # print("lightboard_config:")
+    # pprint(lightboard_config.overall.__dict__)
     # TODO:
     # check if lightboard config is not None
     # pull data for lightboard_config.overall.projects
@@ -34,24 +66,27 @@ def get_tc_builds(args):
     # dedupe by build id
     # filter out lightboard_config.overall.ignored_bts
 
-    baseurl = f"https://{args.username}:{args.password}@buildserver.red-gate.com/httpAuth/app/rest/"
-    buildsUrl = f"{baseurl}builds/"
+    allBuilds = []
+    print("sending requests to teamcity: ")
+    for project in lightboard_config.overall.projects:
+        allBuilds.extend(get_tc_builds(
+            args.username, args.password, projectId=project))
+        print(".", end="", flush=True)
+    for buildType in lightboard_config.overall.bts:
+        allBuilds.extend(get_tc_builds(
+            args.username, args.password, buildTypeId=buildType))
+        print(".", end="", flush=True)
 
-    now = datetime.utcnow()
-    weekago = now - timedelta(days=7)
-    sinceDate = weekago.strftime("%Y%m%dT%H%M%SZ")
-
-    buildsResponse = requests.get(
-        buildsUrl, params={"locator": f"count:4000,sinceDate:{sinceDate}"})
-    buildsResult = ET.fromstring(buildsResponse.text)
-    if int(buildsResult.attrib["count"]) == 4000:
-        print("too many build results and paging isn't implemented")
-        exit(1)
+    print("\n")
+    print(f"Got {len(allBuilds)} builds before filtering...")
+    allBuilds = list(filter(
+        lambda b: b.attrib["buildTypeId"] not in lightboard_config.overall.ignored_bts, allBuilds))
+    print(f"... and {len(allBuilds)} after filtering.")
 
     successCount = 0
     failureCount = 0
     otherCount = 0
-    for build in buildsResult.findall("build"):
+    for build in allBuilds:
         if build.attrib["status"] == "SUCCESS":
             successCount += 1
         elif build.attrib["status"] == "FAILURE":
@@ -78,4 +113,4 @@ if __name__ == '__main__':
     parser.add_argument("password", help="domain password for teamcity")
     args = parser.parse_args()
 
-    get_tc_builds(args)
+    get_all_tc_builds(args)
